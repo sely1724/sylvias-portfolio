9bcb92e5835330426529b3b55ce6e53b15ca0b78
Sylvia Ely <sylvianne9417@gmail.com>
2023-03-02 13:38:07 -0800

    working on contact form.  Think it's done, but I can't test because it's not rendering on page.

--- sylvias-portfolio/README.md
+++ sylvias-portfolio/README.md
@@ -22,20 +22,17 @@ SO THAT I can assess whether they're a good candidate for an open position
 
 ```md
 THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
-WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default
-
-WHEN I am presented with the Portfolio section
-THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
 
 WHEN I am presented with the Contact section
-THEN I see a contact form with fields for a name, an email address, and a message
+THEN I see a contact form with fields for a name, , and a message
 WHEN I move my cursor out of one of the form fields without entering text
 THEN I receive a notification that this field is required
 WHEN I enter text into the email address field
 THEN I receive a notification if I have entered an invalid email address
+
 WHEN I am presented with the Resume section
 THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+
 WHEN I view the footer
 THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
 ```
--- sylvias-portfolio/src/components/pages/Contact.js
+++ sylvias-portfolio/src/components/pages/Contact.js
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../App.css";
 
-// TODO: ADD FORM? CSS?
+// THEN I see a contact form with fields for a name, email , and a message
+// WHEN I move my cursor out of one of the form fields without entering text
+// THEN I receive a notification that this field is required
 
-export default function Contact() {
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from "../../utils/helpers";
+
+function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { name, email, message } = formState;
+
+  const checkValid = (e) => {
+    // Based on the input type, we set the state of either email, username, and password
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Email is not valid - please try again");
+      } else {
+        setErrorMessage("");
+      }
+    }
+    // if error message blank, set new state to include the new email value.
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  };
+
+  function checkLength(e) {
+    if (
+      e.target.name === "name" ||
+      e.target.name === "message" ||
+      e.target.name === "email"
+    ) {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
   return (
-    <div className="contactMe">
-      <h1>Contact Me</h1>
+    <div>
+      <p>Hello {name}</p>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onBlur={checkLength}
+          onChange={checkValid}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={name}
+          name="name"
+          onBlur={checkLength}
+          type="text"
+          placeholder="your name"
+        />
+        <input
+          value={message}
+          name="message"
+          onBlur={checkLength}
+          type="message"
+          placeholder="message"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
+
+export default Contact;
