367d61ccbfc7e359c0307847431d0026df47ee16
Sylvia Ely <sylvianne9417@gmail.com>
2023-03-02 16:58:48 -0800

    working on resume download

--- sylvias-portfolio/src/components/pages/Resume.js
+++ sylvias-portfolio/src/components/pages/Resume.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "../../App.css";
+
+//https://stackoverflow.com/questions/60922523/how-to-make-a-pdf-download-on-a-button-click-with-react
 
 export default function Resume() {
   return (
-    <div className="resume">
-      <h1>Resume</h1>
-      <div>RESUME HERE!</div>
+    <div className="downloadContainer">
+      <button className="button">
+        <a
+          className="button"
+          href="../../resume.pdf"
+          download="ResumeOfficial.pdf"
+        >
+          Download Resume
+        </a>
+      </button>
     </div>
   );
 }
