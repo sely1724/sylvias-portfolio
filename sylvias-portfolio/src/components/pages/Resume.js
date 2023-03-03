a994eb1afa1e7f173dc621cc22e1bea2e0a00e49
Sylvia Ely <sylvianne9417@gmail.com>
2023-03-02 17:32:49 -0800

    need to figure out how to download - adding a png just to have an mvp up

--- sylvias-portfolio/src/components/pages/Resume.js
+++ sylvias-portfolio/src/components/pages/Resume.js
@@ -1,20 +1,32 @@
 import React from "react";
 import "../../App.css";
+import resume from "../../resume.png";
 
-//https://stackoverflow.com/questions/60922523/how-to-make-a-pdf-download-on-a-button-click-with-react
+// TODO - ADD ABILITY TO DOWNLAD AND LIST DEV PROFICIENCIES
 
 export default function Resume() {
   return (
     <div className="downloadContainer">
-      <button className="button">
-        <a
-          className="button"
-          href="../../resume.pdf"
-          download="ResumeOfficial.pdf"
-        >
-          Download Resume
-        </a>
-      </button>
+      <h1>Resume</h1>
+      <img src={resume} alt="Sylvia's Resume" />
     </div>
   );
 }
+
+// //https://stackoverflow.com/questions/60922523/how-to-make-a-pdf-download-on-a-button-click-with-react
+
+// export default function Resume() {
+//   return (
+//     <div className="downloadContainer">
+//       <button className="button">
+//         <a
+//           className="button"
+//           href="../../resume.pdf"
+//           download="ResumeOfficial.pdf"
+//         >
+//           Download Resume
+//         </a>
+//       </button>
+//     </div>
+//   );
+// }
